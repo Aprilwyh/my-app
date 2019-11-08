5684836fa993f2dd0969480009a55267c19d3656
weiyanhui <1234@163.com>
2019-11-08 17:19:09 +0800

    添加父子组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IdeaComponent } from './idea/idea.component';
+import { IdeaDetailsComponent } from './idea-details/idea-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IdeaComponent
+    IdeaComponent,
+    IdeaDetailsComponent
   ],
   imports: [
     BrowserModule,
